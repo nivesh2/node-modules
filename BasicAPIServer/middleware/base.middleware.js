a2e1124468fe7434a033bcc2ee5b96d6b83fa6d6
Nivesh Singh <nivesh2@gmail.com>
2016-02-01 00:14:02 +0530

    Middleware added

---
+++ BasicAPIServer/middleware/base.middleware.js
@@ -0,0 +1,24 @@
+'use strict';
+/**
+ * Adding various middleware to app
+ */
+
+module.exports = function(app){
+    const bodyParser = require('body-parser');
+    const logger = require('morgan');
+
+    //throws 400 error to next, if JSON is not valid 
+    app.use(bodyParser.json({
+        strict:true,
+        })); 
+        
+    //parses the url encoded strings    
+    app.use(bodyParser.urlencoded({ 
+        extended:true,
+        }));
+    
+   
+    //logs incoming request in dev pattern 
+    app.use(logger('dev'));
+};
+
