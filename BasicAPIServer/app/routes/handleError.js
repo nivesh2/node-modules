6c558c7681dea3c377bc2a8a36164e14ffccfc4e
Nivesh Singh <nivesh2@gmail.com>
2016-02-10 21:55:08 +0530

    handle error part updated

--- BasicAPIServer/app/routes/handleError.js
+++ BasicAPIServer/app/routes/handleError.js
@@ -1,41 +1,43 @@
 'use strict';
 module.exports = function(app){
     const debug = require('debug')('main:handdleError');
-    
+
     //Error: 404
-    app.use((req,res)=>{
+    app.use((req,res,next)=>{
        debug('Page Not found, Erro: 404');
-       res.status(404).json({
-          success:false,
-          message:'Page not found' 
-       });
+
+       var err = new Error(Page Not Found);
+       err.status = 404;
+       err.name = 'PageNotFound';
+       next(err);
     });
-    
-        
+
+
     // Devlopment error handler
-    if (app.get('env') === 'development') {        
-        app.use(function(err,req,res){
+    if (app.get('env') === 'development') {
+        app.use(function(err,req,res,next){
             debug(`Error name: ${err.name} | message: ${err.message}`);
-            
+
             res.status(err.status || 500).json({
             error:err,
-            message: err.message,             
-            });			
-            
+            message: err.message,
+            trace: err.stack,
+            });
+
         });
-        
+
     }else {
     // Production error handler
-        app.use(function(err, req, res) {
-            debug(`Error status: ${err.status} | message: ${err.message}`);
-            
+        app.use(function(err, req, res,next) {
+            debug(`Error name: ${err.name} | message: ${err.message}`);
+
             res.status(err.status || 500).json({
-            message: err.message,
-            error: {}
+              error: {},
+              message: err.message,
             });
-            
+
         });
-        
+
     }
 
 };
